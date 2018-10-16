c421e9e6da7cecfe17e2a907046e2fe4da31ba5f
enderbenni <enderbenni202@gmail.com>
2018-10-16 14:19:43 +0200

    tried some stuff :(

--- src/cosps/CospsStart.java
+++ src/cosps/CospsStart.java
@@ -7,26 +7,30 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 public class CospsStart extends JPanel {
-private Graphics2D g2d;
+	private Graphics2D g2d;
+
 	/**
 	 * Create the panel.
 	 */
 	public CospsStart() {
+		System.out.println("it was made");
+		repaint();
 	}
 
 	@Override
 	public void paintComponents(Graphics g) {
-		// TODO Auto-generated method stub
+		System.out.println("made was it");
 		super.paintComponents(g);
-		g2d = (Graphics2D)g;
-		printText(g2d,"Hallo und willkommen zu Cosps!");
+		g2d = (Graphics2D) g;
+		g2d.drawOval(100, 100, 100, 100);
+		printText("Hallo und willkommen zu Cosps!");
 	}
 
-	private void printText(Graphics g2d,String string) {
+	private void printText(String string) {
 		System.out.println("ew");
-		g2d.setFont(new Font("Times New Roman",100,Font.BOLD));
+		g2d.setFont(new Font("Times New Roman", 100, Font.BOLD));
 		g2d.drawString(string, 10, 10);
 		System.out.println("madde it");
 	}
-	
+
 }
